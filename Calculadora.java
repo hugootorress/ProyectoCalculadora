b10fe50c68228428b22f3c4ab1f2fb706b9a396b
hugootorress <hugales2005@gmail.com>
2024-02-12 19:04:46 +0100

    Implementación de la funcionalidad de división

--- Calculadora.java
+++ Calculadora.java
@@ -2,8 +2,17 @@ public class Calculadora {
     public int suma(int a, int b) {
         return a + b;
     }
-    
+
     public int multiplicar(int a, int b) {
         return a * b;
+    }
+
+    public double dividir(double a, double b) {
+        if (b != 0) {
+            return a / b;
+        } else {
+            System.out.println("No se puede dividir entre cero.");
+            return Double.NaN;
+        }
     }
 }
